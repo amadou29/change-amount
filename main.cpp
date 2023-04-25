0df7086741fd36038dfd894a7400a90a4debcc69
amadou29 <97330972+amadou29@users.noreply.github.com>
2023-04-24 20:11:39 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,36 @@
 // Section 8 Challenge
 /*
- * for this program I will be using UD dollars and cents.
- * 
- * Write a program that asks the user to enter the following:
- * An integers representing the number of cents
- * 
- * You may assume that the number of cents entered is greater than or equal to zero
- * 
- * The program should then display how to provide that change to the user.
- * 
- * In the US:
- * 1 dollar is 100 cents
- * 1 quarter is 25 cents
- * 1 dime is 10 cents
- * 1 nickel is 5 cents, and
- * 1 penny is 1 cent
- * 
- * Here is a sample run:
- * 
- * Enter an amount in cents: 92
- * 
- * You can provide this change as follows:
- * dollars: 0
- * quarters: 3
- * dimes: 1
- * nickels: 1
- * pennies: 2
- * 
- * Feel free to use your own currency system.
- * Also, think of how you ight solve the problem using the modulo operator
- * 
- * Have fun and test your program!!
+  For this program I will be using UD dollars and cents.
+  
+  Write a program that asks the user to enter the following:
+  An integers representing the number of cents
+  
+  You may assume that the number of cents entered is greater than or equal to zero
+  
+  The program should then display how to provide that change to the user.
+  
+  In the US:
+  1 dollar is 100 cents
+  1 quarter is 25 cents
+  1 dime is 10 cents
+  1 nickel is 5 cents, and
+  1 penny is 1 cent
+  
+  Here is a sample run:
+  
+  Enter an amount in cents: 92
+  
+  You can provide this change as follows:
+  dollars: 0
+  quarters: 3
+  dimes: 1
+  nickels: 1
+  pennies: 2
+  
+  Feel free to use your own currency system.
+  Also, think of how you ight solve the problem using the modulo operator
+  
+  Have fun and test your program!!
  */
 
 #include <iostream>
